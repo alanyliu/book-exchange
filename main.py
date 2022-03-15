66b905d7aa1952270f6fbdaa430282568a16a41c
alanyliu <82200812+alanyliu@users.noreply.github.com>
2022-03-15 00:38:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Alan Liu // Rice University
+# Alan Liu | Rice University '25
 # /Users/alanliu/PycharmProjects/BookExchange/main.py
 # Created 16-Dec-2021
 # Code that parses through the master book list Google sheet(s) for the BISV Book Exchange organization.
@@ -11,6 +11,12 @@
 # To-do:
 #   - set_price_with_bookfinder
 #   - More user-friendly (flexible options, other get/set methods if nec.)
+
+# Useful websites:
+# https://towardsdatascience.com/read-data-from-google-sheets-into-pandas-without-the-google-sheets-api-5c468536550
+# https://realpython.com/python-requests/
+# https://medium.com/daily-python/python-script-to-edit-google-sheets-daily-python-7-aadce27846c0
+
 
 import pandas as pd
 import math
@@ -27,14 +33,12 @@ class BuyingFormReader:
         :param url: Google sheets book buying form url
         :return: df_final: DataFrame of data from Google sheets
         """
-        # # title (2021) vs Book Title (2020)
-        # # customDate (2021) not found in 2020
-        # init_data = {'isbn': [], 'author': [], 'bookTitle': [], 'price': [], 'copyright': [], 'genre': [],
-        #              'format': [], 'collection': [], 'customDate': []}
-
+        # title (2021) vs Book Title (2020)
+        # customDate (2021) not found in 2020
         df_init = pd.read_csv(url)
-        df_final = df_init([['isbn', 'author', 'title', 'copyright', 'publisher', 'genre', 'collection',
-                             'price', 'sold_at']])
+        df_final = df_init([['ISBN', 'Book Name', 'Author', 'NewPriceL', 'NewPriceH', 'NewPriceMN', 'NewPriceMD',
+                             'GoodPriceL', 'GoodPriceH', 'GoodPriceMN', 'GoodPriceMD', 'FairPriceL', 'FairPriceH',
+                             'FairPriceMN', 'FairPriceMD']])
         return df_final
 
     def get_price_of_book(self, isbn):
@@ -44,7 +48,7 @@ class BuyingFormReader:
         :return: price of book OR an input call if entered ISBN not found
         """
         idx = 0
-        for elem in self.df['isbn']:
+        for elem in self.df['ISBN']:
             # if self.df.at[idx, 'price'] == isbn:
             if elem == isbn:
                 break
@@ -61,7 +65,7 @@ class BuyingFormReader:
         :return: N/A OR an input call if entered ISBN not found
         """
         idx = 0
-        for elem in self.df['isbn']:
+        for elem in self.df['ISBN']:
             if elem == isbn:
                 break
             idx += 1
@@ -69,27 +73,27 @@ class BuyingFormReader:
             return self.get_price_of_book(input("ISBN not found, please try again:"))
         self.df.at[idx, 'price'] = new_price
 
-    def set_price_with_bookfinder(self, isbn, is_new, price_list_idx):
+    def set_price_with_bookfinder(self, isbn, quality):
         """
         Sets price of book based on quality of book and which item down the list on BookFinder to set price.
         :param isbn: ISBN of book in DataFrame
-        :param is_new: boolean representing if book is new
+        :param quality: boolean representing if book is new
         :param price_list_idx: number item on new/old bookfinder list to use that price
         :return:
         """
-        # use math.round()?
+        # to utilize data on quality of book + low/high/mean/median of pricings on BookFinder
         return
 
-    def add_new_book(self, isbn, is_new, collection):
+    def add_new_book(self, isbn, is_new, title):
         """
         Adds new book entry with relevant columns including price, ISBN, etc. Scrape from BookFinder.
         :param isbn: ISBN of book in DataFrame
         :param is_new: boolean representing if book is new
-        :param collection: designated owner of the book with ID in 'ID owner_name' format
+        :param title: string representing book title # designated owner of the book with ID in 'ID owner_name' format
         :return: N/A
         """
-        self.df.loc[len(self.df.index)] = [isbn, '', '', '', '', '', collection, 0, 0]
-        self.set_price_with_bookfinder(isbn, is_new, 1)
+        self.df.loc[len(self.df.index)] = [isbn, title, '', '', '', '', '', '', '', '', '', '', '', '', '']
+        # self.set_price_with_bookfinder(isbn, is_new, 1)
         return
 
 
@@ -102,13 +106,14 @@ def get_bool(prompt):
 
 
 # 2021 master book list
-sheet_id = "1dCgnjSlQAD8eJQxSKb85Eiul2ANbnlov3yg7w1TLmDw"
-sheet_name = "Master book list 2021"
+sheet_id = "1EXGdFpMzucnnHVIwVRnEh8onE5Gr82aZi7wD-lWb2ww"  # "1dCgnjSlQAD8eJQxSKb85Eiul2ANbnlov3yg7w1TLmDw"
+sheet_name = "BookPricing Tab"
 url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
 
 BFR = BuyingFormReader(url)
 BFR.read_form_data(url)
 
+# User input
 res = input("Enter 'book price' to find a book price, 'manual set' to set book price manually, "
             "'set price' to set book price based on BookFinder, or 'add book' to add new book.")
 # find book price using ISBN
@@ -122,14 +127,16 @@ elif res == "manual set":
     BFR.set_price_of_book(set_isbn, updated_price)
 elif res == "set price":
     set_isbn = input("Enter an ISBN number for a book to set new price:")
-    new_or_old = get_bool("'True' if book is new, 'False' otherwise:")
-    bookfinder_idx = input("Enter which book down the BookFinder list to set price to (default 1):")
-    BFR.set_price_with_bookfinder(set_isbn, new_or_old, bookfinder_idx)
+    quality = input("Enter 'New' if book is new, 'Good' if book is in good quality, and 'Fair' if book is in average "
+                    "quality")
+    # bookfinder_idx = input("Enter which book down the BookFinder list to set price to (default 1):")
+    BFR.set_price_with_bookfinder(set_isbn, quality)
 elif res == "add book":
     add_isbn = input("Enter the ISBN number for the book to add:")
     new_or_old = get_bool("'True' if book is new, 'False' otherwise:")
-    owner_info = input("Enter owner ID + name separated by a space:")
-    BFR.add_new_book(add_isbn, new_or_old, owner_info)
+    book_name = input("Enter the name of the book:")
+    # owner_info = input("Enter owner ID + name separated by a space:")
+    BFR.add_new_book(add_isbn, new_or_old, book_name)
 else:
     input("Input did not match any of the acceptable prompts, please try again."
           "Enter 'book price' to find a book price, 'manual set' to set book price manually, "
