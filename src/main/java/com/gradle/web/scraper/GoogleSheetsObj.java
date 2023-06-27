8d2d1207dbd669c2fb3ff1fd379e4569868fa731
Alan Liu <82200812+alanyliu@users.noreply.github.com>
2023-06-27 00:59:44 -0700

    update readbookisbns + work on google sheets update

--- src/main/java/com/gradle/web/scraper/GoogleSheetsObj.java
+++ src/main/java/com/gradle/web/scraper/GoogleSheetsObj.java
@@ -3,7 +3,7 @@ package com.gradle.web.scraper;
 import java.util.*;
 
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.IOException;
@@ -11,7 +11,7 @@ import java.security.GeneralSecurityException;
 
 public class GoogleSheetsObj {
     private static Sheets sheetsService;
-    private static final String SPREADSHEET_ID = "**********";
+    private static final String SPREADSHEET_ID = "***********";
 
     public static void setup() throws IOException, GeneralSecurityException {
         sheetsService = SheetsServiceUtil.getSheetsService();
@@ -19,16 +19,27 @@ public class GoogleSheetsObj {
 
     public static ValueRange readBookISBNs() throws IOException, GeneralSecurityException {
         setup();
-//        List<String> ranges = Arrays.asList("E1", "E4");
-//        BatchGetValuesResponse readResult = sheetsService.spreadsheets().values()
-//                .batchGet(SPREADSHEET_ID)
-//                .setRanges(ranges)
-//                .execute();
         ValueRange readResult = sheetsService.spreadsheets().values()
-                .get(SPREADSHEET_ID, "A:A")
+                .get(SPREADSHEET_ID, "B:B")
                 .execute();
         int numRows = readResult.getValues() != null ? readResult.getValues().size() : 0;
-        System.out.printf("%d rows were retrieved.", numRows);
+        System.out.printf("%d rows were retrieved.\n", numRows);
         return readResult;
+    }
+
+    public static void updatePriceData(List<List<Double>> priceStats) throws IOException, GeneralSecurityException {
+//        for (int i = 0; i < priceStats.size(); i++) {
+//            List<List<Object>> temp = new ArrayList<>();
+//            for (int j = 0; j < priceStats.get(i).size(); j++) {
+//                temp.add(Arrays.asList(priceStats.get(i).get(j)));
+//            }
+//            ValueRange body = new ValueRange()
+//                    .setValues(temp)
+//                    .setMajorDimension("COLUMNS");
+//            UpdateValuesResponse result = sheetsService.spreadsheets().values()
+//                    .update(SPREADSHEET_ID, "AM", body)
+//                    .setValueInputOption("RAW")
+//                    .execute();
+//        }
     }
 }
